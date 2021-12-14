0253846ea0a6e848da4957561b9b8946f05ab203
Jehoshaphat Egbe <jehoshaphategbe1@gmail.com>
2021-12-14 07:05:38 -0800

    Challenge

--- static/js/script.js
+++ static/js/script.js
@@ -35,8 +35,9 @@ function rpsGame(yourChoice){
    console.log("Computer choice:", botChoice);
    results = decideWinner(humanChoice, botChoice); // [0, 1]
    console.log(results);
- // message = finalMessage(results) // ['message': you won, 'color': green];
-// rpsFrondEnd(yourChoice.id, botChoice, message);
+   message = finalMessage(results);
+   console.log(message); // ['message': you won, 'color': green];
+   rpsFrontEnd(yourChoice.id, botChoice, message);
 
 }
 
@@ -53,7 +54,7 @@ function decideWinner(yourChoice, computerChoice) {
     var rpsDatabase = {
         'rock' : {'scissors': 1, 'rock': 0.5, 'paper': 0},
         'paper' : {'rock': 1, 'paper': 0.5, 'scissors': 0},
-        'scisors' : {'paper': 1, 'scisors': 0.5, 'rock': 0},
+        'scissors' : {'paper': 1, 'scissors': 0.5, 'rock': 0}
     }
 
     var yourScore = rpsDatabase[yourChoice][computerChoice];
@@ -61,4 +62,97 @@ function decideWinner(yourChoice, computerChoice) {
 
 
     return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore == 0) {
+        return {'message': 'you lost!', 'color': 'red'};
+        
+    } else if(yourScore == 0.5) {
+        return {'message': 'you tied!', 'color': 'yellow'};
+    }else{
+        return {'message': 'you won!', 'color': 'green'};
+    }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
+    var imagesDatabase = {
+        'rock' : document.getElementById('rock').src,
+        'paper' : document.getElementById('paper').src,
+        'scissors' : document.getElementById('scissors').src
+
+    }
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height= 150 width=150 style='box-shadow:0px 10px 50px rgba(37, 50, 233, 1);'>"
+    messageDiv.innerHTML = "<h1 style='color:" + finalMessage['color'] + "' font-size: 60px; padding: 30px;'>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "'height= 150 width=150 style='box-shadow:0px 10px 50px rgba(243, 38, 24, 1);'>"
+
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+
+}
+
+
+// Challenge 4: Change the Color of All Buttons!
+var all_buttons = document.getElementsByTagName('button');
+// console.log(all_buttons);
+
+
+var copyAllButtons = [];
+for (let i=0; i < all_buttons.length; i++) {
+  copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+ console.log(copyAllButtons);
+
+function buttonColorChange(buttonThingy) {
+  if (buttonThingy.value == 'red') {
+      buttonRed();
+  } else if (buttonThingy.value == 'green') {
+      buttonGreen();
+  }else if (buttonThingy.value == 'reset') {
+      buttonColorReset();
+  }else if (buttonThingy.value == 'random') {
+    randomColors();
+  }
+  // console.log(buttonThingy.value);
+}
+
+function buttonRed() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonGreen() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonColorReset() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors() {
+  var choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning']
+    for (let i=0; i < all_buttons.length; i++) {
+      var randomNumber = Math.floor(Math.random() * 4);
+      all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+      all_buttons[i].classList.add(choices[randomNumber]);
+    }
 }
